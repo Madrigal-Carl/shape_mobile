4d221d340a71b3280dec7a95327061c5da7ffb7b
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-11-14 22:28:55 +0800

    added casual games

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,28 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shape_mobile/widgets/greeting.dart';
 import 'package:shape_mobile/widgets/recent_lesson.dart';
-import 'package:shape_mobile/widgets/game_collection.dart';
+import 'package:shape_mobile/widgets/casual_game_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> games = [
-      {"image": "assets/flutter/games/find_items.png", "title": "Flappy Bird"},
-      {"image": "assets/flutter/games/puzzle.png", "title": "Puzzle"},
-      {
-        "image": "assets/flutter/games/coloring.png",
-        "title": "Creative Coloring",
-      },
-      {
-        "image": "assets/flutter/games/trace.png",
-        "title": "Trace the alphabet",
-      },
-      {"image": "assets/flutter/games/self_care.png", "title": "Self Care"},
-      {"image": "assets/flutter/games/make_a_pair.png", "title": "Make a pair"},
-    ];
-
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -32,7 +17,9 @@ class HomeScreen extends StatelessWidget {
             children: [
               const GreetingWidget(),
               const RecentLessonWidget(),
-              // GameCollectionWidget(games: games, title: 'Casual Games'),
+              CasualGamesSection(
+                games: [CasualGame(id: 1, title: "Count Quest")],
+              ),
             ],
           ),
         ),
