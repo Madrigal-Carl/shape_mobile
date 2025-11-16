92499b3cd02f1ec968f34dec7a59eecb8d88826a
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-11-16 20:40:36 +0800

    change logo

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -113,7 +113,7 @@ class _LoginScreenState extends State<LoginScreen> {
           children: [
             SizedBox(height: MediaQuery.of(context).size.height * 0.20),
             Image.asset(
-              'assets/flutter/images/shape_logo.png',
+              'assets/flutter/images/logo.png',
               width: MediaQuery.of(context).size.width * 0.5,
               fit: BoxFit.cover,
             ),
